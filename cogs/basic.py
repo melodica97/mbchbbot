2f3d811b02771fb49316da79808afbcca6c16307
Melodica97 <shirousagi905254@gmail.com>
2020-04-05 14:46:48 +1200

    beginnings

---
+++ cogs/basic.py
@@ -0,0 +1,80 @@
+import discord
+from discord.ext import commands
+import random
+
+class Basic(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    #Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is ready.')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f'{member} has joined a server.')
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        print(f'{member} has left the server.')
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.client.user:
+            pass
+        
+        if message.content.upper().startswith('I AM') or message.content.upper().startswith("I'M") or message.content.upper().startswith("IM"):
+            l = message.content.upper()
+            mapping = [('I AM',''), ("I'M",''), ('IM','')]
+            message.author
+            for k, v in mapping:
+                l = l.replace(k, v)
+            if len(l) == 0:
+                await message.channel.send("Hi no name, I'm Stove!")
+                await message.author.edit(nick="no name")
+            else:
+                await message.channel.send('Hi' + message.content[-len(l):] + ", I'm Dr. Bot!")
+                await message.author.edit(nick="{}".format(message.content[-len(l):]))
+        #print(message.author.id)
+
+    #Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
+
+    @commands.command(aliases=['8ball'])
+    async def _8ball(self, ctx, *, question):
+        responses = ['It is certain.',
+                    'It is decidedly so.',
+                    'Without a doubt.',
+                    'Yes - definitely.',
+                    'You may rely on it.',
+                    'Most likely.',
+                    'Outlook good.',
+                    'Yes.',
+                    'Signs point to yes.',
+                    'Reply hazy, try again.',
+                    'Ask again later.',
+                    'Better not tell you now.',
+                    'Cannot predict now.',
+                    'Concentrate and ask again.',
+                    'Dont count on it.',
+                    'My reply is no.',
+                    'My sources say no.',
+                    'Outlook not so good.',
+                    'Very doubtful.']
+        await ctx.send('Question: {}\nAnswer: {}'.format(question,random.choice(responses)))
+
+    @commands.command()
+    async def joined(self, ctx, *, member: discord.Member):
+        await ctx.send('{0} joined on {0.joined_at}'.format(member))
+    
+    @commands.command()
+    async def ppsize(self, ctx, member: discord.Member):
+        print(member.id)
+        await ctx.send('The size of your pp is: {}cm'.format((random.uniform(1,6))))
+
+def setup(client):
+    client.add_cog(Basic(client))
---
+++ drbot.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 22:40:42 2020
+
+@author: steve
+"""
+import discord
+from discord.ext import commands
+print(discord.__version__)
+
+import os
+
+client = commands.Bot(command_prefix = '.')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension('cogs.{}'.format(extension))
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension('cogs.{}'.format(extension))
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension('cogs.{}'.format(extension))
+    client.load_extension('cogs.{}'.format(extension))
+
+for filename in os.listdir(r'.\cogs'):
+    print(filename)
+    if filename.endswith('.py'):
+        client.load_extension('cogs.{}'.format(filename[:-3]))
+
+client.run('Njk2MTYyMTEwMTQ4NzA2Mzg0.XoktiQ.yAecUP9kTYJe7YzwZsoEHdbi9Fo')
